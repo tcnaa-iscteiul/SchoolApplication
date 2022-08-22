8718be02cd6473f79dfc0c08652500370f5cae58
tatiana-clim <43815866+tcnaa-iscteiul@users.noreply.github.com>
2022-08-22 11:27:39 +0100

    Update main.ts

--- Api/src/main.ts
+++ Api/src/main.ts
@@ -1,9 +1,23 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { cors: true });
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+        next();
+    });
+
+    app.enableCors({
+        allowedHeaders: "*",
+        origin: "*"
+    });
+    
     app.useGlobalPipes(new ValidationPipe());
     await app.listen(3333);
 }
