aa9c2f7360153f70d5a3ebc6e4e9d83024b0c978
tcnaa <tcnaa@iscte-iul.pt>
2022-08-22 23:48:58 +0100

    vercel.json

--- Api/src/main.ts
+++ Api/src/main.ts
@@ -12,7 +12,31 @@ async function bootstrap() {
         res.header('Access-Control-Allow-Headers', '*');
         next();
     });*/
-    
+
+    const allowCors = fn => async (req, res) => {
+        res.setHeader('Access-Control-Allow-Credentials', true)
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        // another common pattern
+        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        )
+        if (req.method === 'OPTIONS') {
+            res.status(200).end()
+            return
+        }
+        return await fn(req, res)
+    }
+
+    const handler = (req, res) => {
+        const d = new Date()
+        res.end(d.toString())
+    }
+
+    module.exports = allowCors(handler)
+  /*  
     app.use(function (req, res, next) {
 
         // Website you wish to allow to connect
@@ -36,7 +60,7 @@ async function bootstrap() {
         allowedHeaders: "*",
         origin: "*"
     });
-    
+    */
     app.useGlobalPipes(new ValidationPipe());
     await app.listen(process.env.PORT || 3000, function () {
         console.log('CORS-enabled web server listening on port 80')
