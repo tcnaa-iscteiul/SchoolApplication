1702f408967d993f6de4d8d56669f32aa541f419
tcnaa <tcnaa@iscte-iul.pt>
2022-08-22 23:41:34 +0100

    Headers

--- Api/dist/main.js
+++ Api/dist/main.js
@@ -5,6 +5,13 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.use(function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        next();
+    });
     app.enableCors({
         allowedHeaders: "*",
         origin: "*"
--- Api/src/main.ts
+++ Api/src/main.ts
@@ -7,15 +7,31 @@ async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
     /*
     app.use((req, res, next) => {
-        console.log(req);
-        console.log(res);
-        console.log(next);
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', '*');
         res.header('Access-Control-Allow-Headers', '*');
         next();
+    });*/
+    
+    app.use(function (req, res, next) {
+
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', '*');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+
+        // Pass to next layer of middleware
+        next();
     });
-    */
+    
     app.enableCors({
         allowedHeaders: "*",
         origin: "*"
