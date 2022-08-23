f16d22e1d1b4749e1538be8b2ea609e86ca14c64
tcnaa <tcnaa@iscte-iul.pt>
2022-08-23 18:23:35 +0100

    .

--- src/services/api.ts
+++ src/services/api.ts
@@ -24,7 +24,7 @@ const options = {
 };
 
 axios.request(options).then(function (response) {
-    //console.log(response.data);
+    console.log(response.data);
 }).catch(function (error) {
     console.error(error);
 });
