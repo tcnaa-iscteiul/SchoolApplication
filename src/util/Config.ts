801ccb46a64cc5c0369297bbf9324d6c537214a2
tatiana-clim <43815866+tcnaa-iscteiul@users.noreply.github.com>
2022-08-24 00:29:10 +0100

    Update Config.ts

--- src/util/Config.ts
+++ src/util/Config.ts
@@ -1,5 +1,5 @@
 const Config = {
-    API_URL: process.env.NODE_ENV === "development" ? "http://localhost:3333/" :"https://school-application2.vercel.app/",
+    API_URL: process.env.NODE_ENV === "development" ? "http://localhost:3333/" :"https://nest-backend-teste.vercel.app/",
     TIMEOUT_REQUEST: 5000,
     HEADER_REQUEST: {
         Accept: 'application/json'
