4a6c8d4799045593ee341f8b33acbde78c9de8f0
tatiana-clim <43815866+tcnaa-iscteiul@users.noreply.github.com>
2022-08-24 02:45:54 +0100

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,43 +5,20 @@ import Config from '../util/Config';
 const api = axios.create({
     baseURL: `${Config.API_URL}`,
     headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + getCookie("token"),
-        withCredentials: true,
-        mode: 'no-cors',
-    } });
-
-const options = {
-    method: 'GET',
-    url: `${Config.API_URL}user/all`,
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + getCookie("token"),
-        withCredentials: true,
-        mode: 'no-cors',
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + getCookie("token"),
     }
-};
-
-axios.request(options).then(function (response) {
-    console.log(response.data);
-}).catch(function (error) {
-    console.error(error);
 });
 
 
 // For GET requests
 api.interceptors.request.use(
-    
     async (config) => {
         // Add configurations here
         config.headers!["Authorization"] = 'Bearer ' + getCookie("token");
-      //  config.headers!['content - type'] = 'application/x-www-form-urlencoded';
         return config;
     },
     (err) => {
-        console.log(err);
         return Promise.reject(err);
     }
 );
@@ -51,32 +28,30 @@ api.interceptors.response.use(response => {
     return response
 }, err => {
     return new Promise((resolve, reject) => {
-       
         const originalReq = err.config;
         if (err.response.status === 401 && err.config && !err.config._retry) {
             originalReq._retry = true;
             const token = getCookie("token");
             if (token) {
-               originalReq.headers!["Authorization"] = 'Bearer ' + getCookie("token");
+                originalReq.headers!["Authorization"] = 'Bearer ' + getCookie("token");
                 let res = api.put('token/refresh', { oldToken: token })
                     .then((res) => {
                         setCookie("token", res.data.accessToken);
                         originalReq.headers["Authorization"] = `Bearer ${res.data.accessToken}`;
                         return axios(originalReq);
                     })
-                console.log("post");
                 resolve(res)
             }
             else {
                 reject(err)
             }
-        }if (err.response.status === 408 || err.code === 'ECONNABORTED') {
+        } if (err.response.status === 408 || err.code === 'ECONNABORTED') {
             return Promise.reject(err);
         } else {
             reject(err)
         }
-        
-       
+
+
     })
 })
 
