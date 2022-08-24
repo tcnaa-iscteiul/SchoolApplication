44e7f63b2a5b1fb7d320f13396867615aa2d014b
tcnaa <tcnaa@iscte-iul.pt>
2022-08-24 10:26:46 +0100

    .

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,52 +5,17 @@ import Config from '../util/Config';
 const api = axios.create({
     baseURL: `${Config.API_URL}`,
     headers: {
-<<<<<<< HEAD
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + getCookie("token"),
-        withCredentials: true,
-        mode: 'no-cors',
-    }
-});
-
-const options = {
-    method: 'GET',
-    url: `${Config.API_URL}user/all`,
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + getCookie("token"),
-        withCredentials: true,
-        mode: 'no-cors',
-    }
-};
-
-axios.request(options).then(function (response) {
-    console.log(response.data);
-}).catch(function (error) {
-    console.error(error);
-=======
         Accept: 'application/json',
         Authorization: 'Bearer ' + getCookie("token"),
     }
->>>>>>> 4a6c8d4799045593ee341f8b33acbde78c9de8f0
 });
 
 
 // For GET requests
 api.interceptors.request.use(
-<<<<<<< HEAD
-
     async (config) => {
         // Add configurations here
         config.headers!["Authorization"] = 'Bearer ' + getCookie("token");
-        //  config.headers!['content - type'] = 'application/x-www-form-urlencoded';
-=======
-    async (config) => {
-        // Add configurations here
-        config.headers!["Authorization"] = 'Bearer ' + getCookie("token");
->>>>>>> 4a6c8d4799045593ee341f8b33acbde78c9de8f0
         return config;
     },
     (err) => {
@@ -63,17 +28,13 @@ api.interceptors.response.use(response => {
     return response
 }, err => {
     return new Promise((resolve, reject) => {
-<<<<<<< HEAD
-
-=======
->>>>>>> 4a6c8d4799045593ee341f8b33acbde78c9de8f0
         const originalReq = err.config;
         if (err.response.status === 401 && err.config && !err.config._retry) {
             originalReq._retry = true;
             const token = getCookie("token");
             if (token) {
                 originalReq.headers!["Authorization"] = 'Bearer ' + getCookie("token");
-                let res = api.put('token/refresh', { oldToken: token })
+                let res = api.put(`${Config.API_URL}token/refresh`, { oldToken: token })
                     .then((res) => {
                         setCookie("token", res.data.accessToken);
                         originalReq.headers["Authorization"] = `Bearer ${res.data.accessToken}`;
@@ -85,6 +46,7 @@ api.interceptors.response.use(response => {
                 reject(err)
             }
         } if (err.response.status === 408 || err.code === 'ECONNABORTED') {
+            console.log(`A timeout happend on url ${Config.API_URL}`);
             return Promise.reject(err);
         } else {
             reject(err)
--- src/util/Config.ts
+++ src/util/Config.ts
@@ -1,14 +1,13 @@
 const Config = {
-<<<<<<< HEAD
+
     //API_URL: process.env.NODE_ENV === "development" ? "http://localhost:3333/" :"https://nest-backend-teste.vercel.app/",
     API_URL:  "https://nest-backend-teste.vercel.app/",
-=======
-    API_URL: process.env.NODE_ENV === "development" ? "http://localhost:3333/" :"https://school-application.vercel.app/",
->>>>>>> 4a6c8d4799045593ee341f8b33acbde78c9de8f0
+   // API_URL: process.env.NODE_ENV === "development" ? "http://localhost:3333/" :"https://school-application.vercel.app/",
     TIMEOUT_REQUEST: 5000,
     HEADER_REQUEST: {
         Accept: 'application/json'
     }
+
 }
 export default Config;
 
