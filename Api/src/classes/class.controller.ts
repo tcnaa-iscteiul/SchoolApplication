3119badaf6059831dc717443da3dc83775476ebb
tcnaa <tcnaa@iscte-iul.pt>
2022-08-26 18:55:07 +0100

    -Modified create student component

--- Api/src/classes/class.controller.ts
+++ Api/src/classes/class.controller.ts
@@ -1,12 +1,12 @@
 import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Query,
-  Delete,
-  Patch,
-  UseGuards,
+    Body,
+    Controller,
+    Get,
+    Post,
+    Query,
+    Delete,
+    Patch,
+    UseGuards,
 } from '@nestjs/common';
 import { TeacherToClassDto } from './dto/AssignTeacherToClass.dto';
 import { ClassCreateDto } from './dto/ClassCreate.dto';
@@ -22,66 +22,66 @@ import { Role } from '../users/dto/UserRole.dto';
 @UseGuards(AuthGuard('jwt'))
 @Controller('class')
 export class ClassController {
-  constructor(private classService: ClassService) {}
+    constructor(private classService: ClassService) { }
 
-  @Roles(Role.Admin)
-  @Post()
-  async create(@Body() classCreateDto: ClassCreateDto): Promise<void> {
-    return await this.classService.create(classCreateDto);
-  }
-
-  @Get('/all')
-  async getAll(@Query() param: ClassSearchDto): Promise<Class[]> {
-    if (Object.keys(param).length) {
-      return this.classService.classSearch(param);
-    } else {
-      return this.classService.getAll();
+    @Roles(Role.Admin)
+    @Post()
+    async create(@Body() classCreateDto: ClassCreateDto): Promise<void> {
+        return await this.classService.create(classCreateDto);
     }
-  }
 
-  @Roles(Role.Admin)
-  @Patch()
-  async updateClass(@Body() classUpdateDto: ClassUpdateDto): Promise<void> {
-    return await this.classService.update(classUpdateDto);
-  }
+    @Get('/all')
+    async getAll(@Query() param: ClassSearchDto): Promise<Class[]> {
+        if (Object.keys(param).length) {
+            return this.classService.classSearch(param);
+        } else {
+            return this.classService.getAll();
+        }
+    }
 
-  @Roles(Role.Admin)
-  @Delete()
-  async deleteClass(@Query() param: ClassUpdateDto) {
-    return await this.classService.delete(param);
-  }
+    @Roles(Role.Admin)
+    @Patch()
+    async updateClass(@Body() classUpdateDto: ClassUpdateDto): Promise<void> {
+        return await this.classService.update(classUpdateDto);
+    }
 
-  @Roles(Role.Admin)
-  @Patch('/student')
-  async assignStudentsToClass(@Body() classSearchDto: StudentToClassDto) {
-    return await this.classService.assignStudentsToClass(classSearchDto);
-  }
+    @Roles(Role.Admin)
+    @Delete()
+    async deleteClass(@Query() param: ClassUpdateDto) {
+        return await this.classService.delete(param);
+    }
 
-  @Roles(Role.Admin)
-  @Patch('/removeStudent')
-  async removeStudentsFromClass(@Body() classSearchDto: StudentToClassDto) {
-    return await this.classService.removeStudentsFromClass(classSearchDto);
-  }
+    @Roles(Role.Admin)
+    @Patch('/student')
+    async assignStudentsToClass(@Body() classSearchDto: StudentToClassDto) {
+        return await this.classService.assignStudentsToClass(classSearchDto);
+    }
 
-  @Roles(Role.Admin)
-  @Patch('/assign')
-  async assignTeacherToClass(@Body() classSearchDto: TeacherToClassDto) {
-    return await this.classService.assignTeacherToClass(classSearchDto);
-  }
+    @Roles(Role.Admin)
+    @Patch('/removeStudent')
+    async removeStudentsFromClass(@Body() classSearchDto: StudentToClassDto) {
+        return await this.classService.removeStudentsFromClass(classSearchDto);
+    }
 
-  @Roles(Role.Admin)
-  @Patch('/remove')
-  async removeTeacherFromClass(@Body() classSearchDto: TeacherToClassDto) {
-    return await this.classService.removeTeacherFromClass(classSearchDto);
-  }
+    @Roles(Role.Admin)
+    @Patch('/assign')
+    async assignTeacherToClass(@Body() classSearchDto: TeacherToClassDto) {
+        return await this.classService.assignTeacherToClass(classSearchDto);
+    }
 
-  @Get('/nr')
-  async getNrClasses() {
-    return await this.classService.getNrClasses();
-  }
+    @Roles(Role.Admin)
+    @Patch('/remove')
+    async removeTeacherFromClass(@Body() classSearchDto: TeacherToClassDto) {
+        return await this.classService.removeTeacherFromClass(classSearchDto);
+    }
 
-  @Get()
-  async getClassByUser() {
-    return await this.classService.getClassByUser();
-  }
+    @Get('/nr')
+    async getNrClasses() {
+        return await this.classService.getNrClasses();
+    }
+
+    @Get()
+    async getClassByUser() {
+        return await this.classService.getClassByUser();
+    }
 }
--- src/components/Admin/CreateStudent.tsx
+++ src/components/Admin/CreateStudent.tsx
@@ -6,46 +6,25 @@ import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useInput from "../../hooks/useInput";
 import { Fragment } from "react";
 import Modal from "../UI/Modal";
-import { Status } from "../../interfaces/Status";
-import { Role } from "../../interfaces/Role";
 import LoadingSpinner from "../UI/LoadingSpinner";
 import { memo } from "react";
 import useAxios from "../../hooks/use-axios";
 import { fetchUsersData } from "../../store/usersActions";
 import { useAppDispatch } from "../../hooks/use-redux";
+import CreateStudentFields from "./CreateStudentFields";
+import { Role } from "../../interfaces/Role";
 
 function CreateStudent() {
     const dispatch = useAppDispatch();
 
     const [showModal, setShowModal] = useState<boolean>(false);
+    const [showFields, setShowFields] = useState<boolean>(false);
+    const [email, setEmail] = useState<string>();
 
-    const letters = /^[A-Za-z]+$/;
-    const {
-        value: enteredFirstName,
-        isValid: enteredFirstNameIsValid,
-        hasError: firstNameInputHasError,
-        valueChangeHandler: firstNameChangedHandler,
-        valueBlurHandler: firstNameBlurHandler,
-        reset: resetFirstNameInput,
-    } = useInput(
-        (value: string) =>
-            value.trim() !== "" && value.length > 2 && letters.test(value)
-    );
-    const {
-        value: enteredLastName,
-        isValid: enteredLastNameIsValid,
-        hasError: lastNameInputHasError,
-        valueChangeHandler: lastNameChangedHandler,
-        valueBlurHandler: lastNameBlurHandler,
-        reset: resetLastNameInput,
-    } = useInput(
-        (value: string) =>
-            value.trim() !== "" && value.length > 2 && letters.test(value)
-    );
     const re =
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const {
@@ -56,31 +35,6 @@ function CreateStudent() {
         valueBlurHandler: emailBlurHandler,
         reset: resetEmailInput,
     } = useInput((value: string) => re.test(value));
-    const number = /^[0-9]+$/;
-    const {
-        value: enteredPhone,
-        isValid: enteredPhoneIsValid,
-        hasError: phoneInputHasError,
-        valueChangeHandler: phoneChangedHandler,
-        valueBlurHandler: phoneBlurHandler,
-        reset: resetPhoneInput,
-    } = useInput(
-        (value: string) =>
-            value.trim() !== "" && value.length === 9 && number.test(value)
-    );
-
-    const generatePassword = (): string => {
-        const chars =
-            "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        const passwordLength = 12;
-        let password = "";
-
-        for (let i = 0; i <= passwordLength; i++) {
-            const randomNumber = Math.floor(Math.random() * chars.length);
-            password += chars.substring(randomNumber, randomNumber + 1);
-        }
-        return password;
-    };
 
     const {
         response,
@@ -88,39 +42,36 @@ function CreateStudent() {
         loading: isLoading,
         sendData,
     } = useAxios({
-        method: "Post",
-        url: "auth/create",
-        data: {
+        method: "Get",
+        url: "user/all",
+        params: {
             email: enteredEmail,
-            password: generatePassword(),
-            status: Status.Active,
-            role: Role.Student,
-            firstName: enteredFirstName,
-            lastName: enteredLastName,
-            phone: enteredPhone,
-        },
+        }
     });
 
     const validateForm: boolean =
-        enteredFirstNameIsValid &&
-        enteredLastNameIsValid &&
-        enteredEmailIsValid &&
-        enteredPhoneIsValid;
+        enteredEmailIsValid
 
     const resetInputs = () => {
-        resetFirstNameInput();
-        resetLastNameInput();
         resetEmailInput();
-        resetPhoneInput();
+        setShowFields(false);
     };
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-
         sendData();
-        setShowModal(true);
-        resetInputs();
     };
+
+    useEffect(() => {
+        if (response?.data.length === 0) {
+            setShowModal(false);
+            setEmail(enteredEmail);
+            setShowFields(true);
+        } else {
+            setShowModal(true);
+            resetEmailInput();
+        }
+    }, [response]);
 
     const handleCloseModal = () => {
         if (!error) {
@@ -132,12 +83,12 @@ function CreateStudent() {
     return (
         <Fragment>
             {isLoading && <LoadingSpinner />}
-            {!isLoading && (
+            {!isLoading && (showModal||error )&&(
                 <Modal
                     open={showModal}
                     onClose={handleCloseModal}
-                    message={error || "Student registered with success"}
-                    title={error ? "error" : "Success"}
+                    message={error || "Email already registered"}
+                    title={"Error"}
                 />
             )}
             <Container component="main" maxWidth="xs">
@@ -146,46 +97,13 @@ function CreateStudent() {
                     <Typography component="h1" variant="h5">
                         Create Student
                     </Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate>
+                    <Box component="form" onSubmit={handleSubmit}>
                         <Grid container spacing={2}>
                             <Grid item xs={12}>
                                 <TextField
-                                    name="firstName"
                                     required
-                                    id="firstName"
-                                    label="First Name"
-                                    autoFocus
-                                    error={firstNameInputHasError}
-                                    value={enteredFirstName}
-                                    onChange={firstNameChangedHandler}
-                                    onBlur={firstNameBlurHandler}
-                                    helperText={
-                                        firstNameInputHasError &&
-                                        enteredFirstName &&
-                                        "Please insert a valid name"
-                                    }
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    id="lastName"
-                                    label="Last Name"
-                                    name="lastName"
-                                    error={lastNameInputHasError}
-                                    value={enteredLastName}
-                                    onChange={lastNameChangedHandler}
-                                    onBlur={lastNameBlurHandler}
-                                    helperText={
-                                        lastNameInputHasError &&
-                                        enteredLastName &&
-                                        "Please insert a valid  last name"
-                                    }
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
+                                    fullWidth
+                                    disabled={showFields}
                                     error={emailInputHasError}
                                     id="email"
                                     label="Email Address"
@@ -201,36 +119,37 @@ function CreateStudent() {
                                     }
                                 />
                             </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    id="phoneNumber"
-                                    label="Phone Number"
-                                    name="phoneNumber"
-                                    error={phoneInputHasError}
-                                    value={enteredPhone}
-                                    onChange={phoneChangedHandler}
-                                    onBlur={phoneBlurHandler}
-                                    helperText={
-                                        phoneInputHasError &&
-                                        enteredPhone &&
-                                        "Please insert a valid phone number"
-                                    }
-                                />
-                            </Grid>
+
                             <Grid item xs={12}>
                                 <Button
                                     fullWidth
                                     type="submit"
                                     variant="contained"
-                                    disabled={!validateForm}
+                                    disabled={!validateForm || showFields}
                                 >
-                                    Create Student
+                                    Search Student
+                                </Button>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Button
+                                    fullWidth
+                                    onClick={() => {
+                                        console.log(showFields);
+                                        setShowFields(false);
+                                    }}
+                                    variant="contained"
+                                    disabled={!showFields}
+                                >
+                                    Edit Email
                                 </Button>
                             </Grid>
                         </Grid>
                     </Box>
                 </Box>
+                {showFields &&
+                    <CreateStudentFields
+                        email={email!}
+                        resetInputs={resetInputs} />}
             </Container>
         </Fragment>
     );
