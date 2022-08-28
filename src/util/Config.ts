793d34f88e9493de7e9380132dc48ce4c19d7d11
tcnaa <tcnaa@iscte-iul.pt>
2022-08-28 04:10:52 +0100

    .

--- src/util/Config.ts
+++ src/util/Config.ts
@@ -2,7 +2,7 @@ const Config = {
   API_URL:
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3333/'
-      : process.env.PORT,
+      : 'https://nest-backend-teste.vercel.app/',
   TIMEOUT_REQUEST: 5000,
   HEADER_REQUEST: {
     Accept: 'application/json',
